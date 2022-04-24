d8ce34eed15f41d13cdfa6a7f5db4705e450a23c
Jeaneth Hernandez <jeaneth.hernandez.r@gmail.com>
2022-04-24 17:55:03 -0500

    Added navbar.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <img
+        src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+        alt="Google Logo"
+      />
+      <div>Homepage</div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
