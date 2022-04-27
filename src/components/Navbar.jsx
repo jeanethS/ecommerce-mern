b99cef00ee2c3d91a0d00774a374d756708547ee
Jeaneth Hernandez <jeaneth.hernandez.r@gmail.com>
2022-04-26 22:34:38 -0500

    Added language and MUI icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,51 @@
+import { Search } from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
 const Container = styled.div`
   height: 60px;
-  backgroud-color: #black;
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
+const Center = styled.div`
+  flex: 1;
 `;
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            input icon
+            <Search />
+          </SearchContainer>
+        </Left>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,8 @@ import Navbar from "../components/Navbar";
 const Home = () => {
   return (
     <div>
-      <img
-        src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-        alt="Google Logo"
-      />
-      <div>Homepage</div>
       <Navbar />
+      <div>Homepage</div>
     </div>
   );
 };
