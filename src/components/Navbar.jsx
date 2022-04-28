759d0822eae32640301426ea021fdc3c31c2fcca
Jeaneth Hernandez <jeaneth.hernandez.r@gmail.com>
2022-04-27 22:24:20 -0500

    Fixed navbar padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,4 @@
-import {
-  Search,
-  SearchOutlined,
-  ShoppingCartOutlined,
-} from "@mui/icons-material";
+import { SearchOutlined, ShoppingCartOutlined } from "@mui/icons-material";
 import { Badge } from "@mui/material";
 import React from "react";
 import styled from "styled-components";
@@ -43,16 +39,19 @@ const Right = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
+  justify-content: flex-end;
 `;
 const Center = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   text-align: center;
+  justify-content: center;
 `;
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
+  margin-left: 25px;
 `;
 
 const Navbar = () => {
@@ -61,7 +60,7 @@ const Navbar = () => {
       <Wrapper>
         <Left>
           <Language>EN</Language>
-          <SearchContainer>
+          <SearchContainer style={{ color: "gray", fontSize: 16 }}>
             <Input />
             <SearchOutlined />
           </SearchContainer>
@@ -72,9 +71,11 @@ const Navbar = () => {
         <Right>
           <MenuItem>REGISTER</MenuItem>
           <MenuItem>LOGIN</MenuItem>
-          <Badge badgeContent={4} color="primary">
-            <ShoppingCartOutlined />
-          </Badge>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
         </Right>
       </Wrapper>
     </Container>
